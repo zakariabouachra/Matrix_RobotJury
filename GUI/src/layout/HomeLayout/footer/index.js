116ca375a7b45f9db5aa0e3eacb39a8d7b63271e
ZAKARIA <zikotato03@gmail.com>
2023-11-13 03:03:23 -0500

    delete files from gui for update

--- GUI/src/layout/HomeLayout/footer/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Footer from "./footer";
-const Appfooter=()=>{
-    return(
-       <Footer/>
-    );
-};
-export default Appfooter;
--- GUI/src/layout/HomeLayout/header/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Header from "./header";
-
-const Appheader=()=>{
-    return(
-        <Header/>
-    );
-};
-export default Appheader;
